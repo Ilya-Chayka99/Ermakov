d52cef63b54b78bd57d9d21e4860886e69c19a68
Ilya-Chayka99 <Ilya-Chayka99@yandex.ru>
2022-11-27 21:11:29 +0400

    к лекции

--- OOPEAB/Start.java
+++ OOPEAB/Start.java
@@ -1,50 +1,33 @@
-import src.City.CitiObr;
-import src.City.City;
-import src.City.Way;
-import src.Gun.Gun;
-import src.Gun.GunAvtomat;
-import src.Shooter.Shooter;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Start {
     public static void main(String[]args) throws Exception {
-
-//        System.out.println(MyUtilits.sum(2,new Fraction(3,5),2.3));
-//        Kykychka k = new Kykychka();
-//        Vorobei b = new Vorobei();
-//        MyUtilits.birdMarket(k,b,k,k,b);
-//        Square s=new Square(1,1,2);
-//        Point p = new Point(1,1);
-//        Rectangle r=new Rectangle(p,2,2);
-//        System.out.println(MyUtilits.squaresFigures(s,r,s));
-//        Cat cat = new Cat("asss");
-//        Cat cat1 = new Cat("asssq");
-//        Cat cat2 = new Cat("asssw");
-//        MyUtilits.Meowing(cat,cat2,cat1);
-//        Line l = new Line(0,0,4,0);
-//        Point p1 = new Point(0,0);
-//        Point p2 = new Point(8,0);
-//        Point p3 = new Point(8,6);
-//        Point p4 = new Point(7,6);
-//        PoliLine p = new PoliLine(p1,p2,p3,p4);
-//        System.out.println(MyUtilits.longLines(l,p));
-//        City c = new City("C");
-//        City b = new City("B");
-//        City e = new City("E");
-//        CitiObr a = new CitiObr("A");
-//        CitiObr d = new CitiObr("D");
-//
-//        a.addWay(new Way(c,2));
-//        a.addWay(new Way(b,1));
-//        d.addWay(new Way(b,4));
-//        b.addWay(new Way(c,10));
-//        e.addWay(new Way(d,7));
-//        e.addWay(new Way(c,6));
-//        d.addWay(new Way(e,3));
-//        System.out.println(c+ " "+a+" "+b+" "+d+" "+e);
-
-
+        Student st = new Student("pete");
+        st.addGrade(1,23,4,5,7);
+        System.out.println(st);
 
     }
+}
+
+
+class Student {
+    private String name;
+    private List<Integer> marks = new ArrayList<>();
+
+    public Student(String name) {
+        this.name = name;
+    }
+    public void addGrade(int... ar){
+        for(int i:ar) marks.add(i);
+    }
+    public List<Integer> getMarks(){
+        return new ArrayList<>(marks);
+    }
+    public String getName() {
+        return name;
+    }
+
 
 }
